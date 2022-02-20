28dab08d2a084dec5e466ebbbcecd9f47eed9ad4
Akash <akkida746@gmail.com>
2022-02-20 07:15:42 +0530

    Apache Camel

---
+++ src/main/java/com/example/demo/Route.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Route extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        restConfiguration()
+                .port(8080)
+                .bindingMode(RestBindingMode.json);
+
+        rest().get("/greeting")
+                .to("/greeting");
+
+
+        from("/greeting")
+                .log("Getting Msg")
+                .bean(String.class,greeting());
+    }
+
+    static String greeting(){
+        return "Hello";
+    }
+}
